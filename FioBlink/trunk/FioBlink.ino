46da9ef19d50d1556c5d3a0c21ff8ce57014cecd
jesusnuma <jesusnuma@328227b3-c6c3-0820-600e-6621f545ad91>
2014-08-26 21:14:28 +0000

    Implement complementary filter

--- FioBlink/trunk/FioBlink.ino
+++ FioBlink/trunk/FioBlink.ino
@@ -25,19 +25,14 @@ LSM303 compass;
 #define	LED	32						// PB5 as LED
 #define gyroGain	0.00875			// Gyroscope Sensitivity factor for 250 dps
 #define accGain		0.05729			// Accelerometer Sensivity factor 1 mg/LSB * (180/Pi)
+#define AA			0.5				// Complementary filter coefficient
 const byte sampleNum =	255;		// Number of samples to be read to perform calibration
-const float deltaTime = 0.020;		// Sample Time = 20ms
+const float deltaTime = 0.060;		// Sample Time = 20ms
 unsigned long now = 0;
 
 //	Gyroscope variables
-int gyro_offsetX = 0;
-int gyro_offsetY = 0;
 int gyro_offsetZ = 0;
-float gyroRateX = 0.0;
-float gyroRateY = 0.0;
 float gyroRateZ = 0.0;
-float gyroAngleX = 0.0;
-float gyroAngleY = 0.0;
 float gyroAngleZ = 0.0;
 
 // Accelerometer variables
@@ -47,18 +42,19 @@ int acc_offsetZ = 0;
 float accX = 0.0;
 float accY = 0.0;
 float accZ = 0.0;
-float roll = 0.0;
 float pitch = 0.0;
-float theta = 0.0;
+
+// Complementary filter
+float angleFilter = 0.0;
 
 //	Function Prototypes
 unsigned char commandInput(void);
 void idle(void);
-void getGyroscopeReadings(void);
+void getImuReadings(void);
+void processData(void);
 void calculateGyroOffset(void);
-void getAccelerometerReadings(void);
 void calculateAccOffset(void);
-void getIMUData(void);
+void displayData(void);
 
 // FINITE STATE MACHINE DEFINITIONS
 
@@ -78,7 +74,7 @@ StateType fsm[4] = {
 	{ &idle,				10,		{ IDLE, CALIBRATE_ACC, CALIBRATE_GYRO, OUTPUT_DATA }},
 	{ &calculateAccOffset,	255,	{ IDLE, CALIBRATE_ACC, CALIBRATE_GYRO, OUTPUT_DATA }},
 	{ &calculateGyroOffset,	255,	{ IDLE, CALIBRATE_ACC, CALIBRATE_GYRO, OUTPUT_DATA }},
-	{ &getIMUData,			0,		{ IDLE, CALIBRATE_ACC, CALIBRATE_GYRO, OUTPUT_DATA }}
+	{ &displayData,			0,		{ IDLE, CALIBRATE_ACC, CALIBRATE_GYRO, OUTPUT_DATA }}
 };
 
 //	General use variables
@@ -137,86 +133,63 @@ void idle() {
 	
 }
 
-void getGyroscopeReadings() {	
+void getImuReadings() {
 	
-	// Read raw data from Gyroscope
+	// Get raw data from accelerometer
+	compass.readAcc();
+	
+	// Get raw data from gyroscope
 	gyro.read();
+}
 
+void processData() {
+	
 	// Calculate angular velocity from Gyro raw data
-	gyroRateX = (float)((int)gyro.g.x - gyro_offsetX) * gyroGain;
-	gyroRateY = (float)((int)gyro.g.y - gyro_offsetY) * gyroGain;
 	gyroRateZ = (float)((int)gyro.g.z - gyro_offsetZ) * gyroGain;
 
 	// Convert angular velocity to angles
-	gyroAngleX += gyroRateX * deltaTime;
-	gyroAngleY += gyroRateY * deltaTime;
 	gyroAngleZ += gyroRateZ * deltaTime;
 	
-	// Keep angles between -180 to 179
-	if (gyroAngleX < -180)			gyroAngleX += 360;
-	else if (gyroAngleX >= 180)		gyroAngleX -= 360;
-	
-	if (gyroAngleY < -180)			gyroAngleY += 360;
-	else if (gyroAngleY >= 180)		gyroAngleY -= 360;
-	
+	// Keep angles between -180 to 179	
 	if (gyroAngleZ < -180)			gyroAngleZ += 360;
 	else if (gyroAngleZ >= 180)		gyroAngleZ -= 360;
-	
-}
-
-void calculateGyroOffset() {
-	
-	// Set variables to 0's
-	gyro_offsetX = 0;
-	gyro_offsetY = 0;
-	gyro_offsetZ = 0;
-	
-	// Perform averaging
-	for (byte n = 0; n < sampleNum; n++) {
-		gyro.read();
-		gyro_offsetX += (int)gyro.g.x;
-		gyro_offsetY += (int)gyro.g.y;
-		gyro_offsetZ += (int)gyro.g.z;
-	}
-	gyro_offsetX /= sampleNum;
-	gyro_offsetY /= sampleNum;
-	gyro_offsetZ /= sampleNum;
-	
-	Serial.print("Gyro Bias: ");
-	Serial.print(gyro_offsetX);
-	Serial.print(", ");
-	Serial.print(gyro_offsetY);
-	Serial.print(", ");
-	Serial.println(gyro_offsetZ);
-	
-}
-
-void getAccelerometerReadings() {
-	
-	// Read raw values of accelerometer
-	compass.readAcc();
 	
 	// Current value -  offset * Accelerometer gain
 	accX = (float)(compass.a.x - acc_offsetX) * accGain;
 	accY = (float)(compass.a.y - acc_offsetY) * accGain;
 	accZ = (float)(compass.a.z - acc_offsetZ) * accGain;
 	
-	// Calculate roll, pitch and theta angles
-	roll = (atan2(accZ, sqrt(accY*accY + accX*accX))) * RAD_TO_DEG;
+	// Calculate angle of X-axis respect to ground
 	pitch = (atan2(accX, sqrt(accY*accY + accZ*accZ))) * RAD_TO_DEG;
-	theta = (atan2(sqrt(accX*accX + accZ*accZ),accY)) * RAD_TO_DEG;
 	
-	// Keep angles between -180 to 179
-	if (roll < -180)			roll += 360;
-	else if (roll >= 180)		roll -= 360;
-	
+	// Keep angles between -180 to 179	
 	if (pitch < -180)			pitch += 360;
 	else if (pitch >= 180)		pitch -= 360;
 	
-	if (theta < -180)			theta += 360;
-	else if (theta >= 180)		theta -= 360;
+	// Perform complementary filter 
+	// Current angle = 50%*(current angle + gyro rotation rate) + 50*(Accelerometer angle)
+	angleFilter = AA * (angleFilter +  gyroRateZ*deltaTime) + (1 - AA)*(pitch);
+	
 	
 }
+
+void calculateGyroOffset() {
+	
+	// Set variable to 0's
+	gyro_offsetZ = 0;
+	
+	// Perform averaging
+	for (byte n = 0; n < sampleNum; n++) {
+		gyro.read();
+		gyro_offsetZ += (int)gyro.g.z;
+	}
+	gyro_offsetZ /= sampleNum;
+	
+	Serial.print("Gyro Bias: ");
+	Serial.println(gyro_offsetZ);
+	
+}
+
 void calculateAccOffset() {
 	
 	// Set variables to 0's
@@ -246,35 +219,29 @@ void calculateAccOffset() {
 	
 }
 
-void getIMUData() {
+void displayData() {
 	
 	// Get current time of system
 	now = millis();
 	
-	// Perform Gyroscope readings
-	getGyroscopeReadings();
+	// Perform Accelerometer and Gyroscope raw readings
+	getImuReadings();
 	
-	// Perform Accelerometer readings
-	getAccelerometerReadings();
+	// Calculate angular velocities and angles
+	processData();
 	
-	// Display Information
-	Serial.write("Gx: ");
-	Serial.print(gyroAngleX);
-	Serial.write("	Gy: ");
-	Serial.print(gyroAngleY);
-	Serial.write("	Gz: ");
+	// Display results
+	Serial.write("Gz: ");
 	Serial.print(gyroAngleZ);
-	Serial.write(10);
-	
-	Serial.write("Roll: ");
-	Serial.print(roll);
+
 	Serial.write("	Pitch: ");
 	Serial.print(pitch);
-	Serial.write("	Theta: ");
-	Serial.print(theta);
+	
+	Serial.write("	Filter: ");
+	Serial.print(angleFilter);
 	Serial.write(10);
 	
-	// Wait 20ms
-	while ( (millis() - now) < 20);
+	// Wait 60ms
+	while ( (millis() - now) < 60);
 	
 }
